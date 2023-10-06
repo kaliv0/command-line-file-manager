620761478446fd882e4789b775ff781043a0ea6f
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-10-06 18:06:08 +0300

    added organize_files function

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,6 @@
 import click
 
-from app.util import dir_scanner
+from app.util import scanner, organizer
 
 
 @click.group()
@@ -8,11 +8,13 @@ def cli():
     pass
 
 
-cli.add_command(dir_scanner.scan_files)
-cli.add_command(dir_scanner.scan_subdirs)
-cli.add_command(dir_scanner.build_catalog)
-cli.add_command(dir_scanner.build_catalog_recursively)
-cli.add_command(dir_scanner.build_tree)
-cli.add_command(dir_scanner.build_pretty_tree)
-cli.add_command(dir_scanner.search_by_name)
-cli.add_command(dir_scanner.search_by_name_recursively)
+cli.add_command(scanner.scan_files)
+cli.add_command(scanner.scan_subdirs)
+cli.add_command(scanner.build_catalog)
+cli.add_command(scanner.build_catalog_recursively)
+cli.add_command(scanner.build_tree)
+cli.add_command(scanner.build_pretty_tree)
+cli.add_command(scanner.search_by_name)
+cli.add_command(scanner.search_by_name_recursively)
+
+cli.add_command(organizer.organize_files)
