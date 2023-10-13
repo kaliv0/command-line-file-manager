0bc8e3b9f5c827a2955ece8aca7f2662ec97f81d
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-10-13 09:42:44 +0300

    added tests to test_organizer and cleaned up code

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,10 @@ RESOURCE_DIR = "tests/resources"
 @pytest.fixture(scope="function")
 def runner(request):
     return CliRunner()
+
+
+@pytest.fixture(scope="function")
+def mock_organizer(monkeypatch):
+    monkeypatch.setattr("shutil.move", lambda src, target: None)
+    monkeypatch.setattr("os.makedirs", lambda target: None)
+    monkeypatch.setattr("os.rmdir", lambda target: None)
